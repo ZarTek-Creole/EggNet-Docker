28ceabd343a24c3d6ee650915c49c622863a3c71
ZarTek-Creole <ZarTek.Creole@gmail.com>
2023-02-04 15:19:30 +0000

    Ajout de lenvois des erreurs sur le botnet

--- app/startup-sequence/tcl/04-bgerror.tcl
+++ app/startup-sequence/tcl/04-bgerror.tcl
@@ -2,9 +2,10 @@ proc bgerror { message } {
 	set timestamp	[clock format [clock seconds]]
 	set FILE_PIPE	[open ${::EGG_PATH_LOGS}/${::EGG_LONG_NAME}_error.log {WRONLY CREAT APPEND}]
 	set LASTPROC	[info level 0]
-	puts $FILE_PIPE "$timestamp: bgerror in $LASTPROC '$message'"
-	foreach err [split $::errorInfo "\n"] {
-		puts $FILE_PIPE $err
+	puts ${FILE_PIPE} "${timestamp}: bgerror in ${LASTPROC} '${message}'";
+	foreach err [split ${::errorInfo} "\n"] {
+		puts ${FILE_PIPE} ${err}
+		putallbots "NET_ERROR ${::EGG_LONG_NAME} ${err}"
 	}
-	close $FILE_PIPE
+	close ${FILE_PIPE}
 }
