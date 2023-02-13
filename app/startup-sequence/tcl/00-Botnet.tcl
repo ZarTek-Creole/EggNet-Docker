c984a09f94fa7f96175253fa374b0f9560887d98
ZarTek-Creole <ZarTek.Creole@gmail.com>
2023-02-13 01:00:48 +0000

    Fix deco/reco tout les 5 minutes

--- app/startup-sequence/tcl/00-Botnet.tcl
+++ app/startup-sequence/tcl/00-Botnet.tcl
@@ -17,7 +17,7 @@ proc userfileloaded { args } {
 			# Handle any errors that may occur
     		putlog "Error userfileloaded: ${error}"
 		} finally {
-			putlog "Ajout du user ${::env(PPL_USER)}"
+			putlog "Ajout du user-master: ${::env(PPL_USER)} réussi"
 		}
 	}
 	# Call the "BOTNET_CREATE" proc
@@ -29,38 +29,40 @@ proc userfileloaded { args } {
 
 proc ::BOTNET_CREATE {} {
 	foreach FILE_SCT [glob -directory ${::EGG_PATH_SECRETS} *.sct] {
-    set B_NAME [file rootname [file tail ${FILE_SCT}]]
-    if { "${::botnet-nick}" == "${B_NAME}" } { continue }
-	set FILE_SCT		[open "${FILE_SCT}" r]
-    set SCT_DATA 		[split [read ${FILE_SCT}]]
-    close ${FILE_SCT}
-    set BOT_ISMASTER	[lindex ${SCT_DATA} 0]
-    set BOT_PASSWORD	[lindex ${SCT_DATA} 1]
-    set BOT_PORT		[lindex ${SCT_DATA} 2]
-    set BOT_HOSTNAME	[lindex ${SCT_DATA} 3]
-    set BOT_LASTMOD		[lindex ${SCT_DATA} 4]
-	putlog "debug : ${SCT_DATA}"
-	putlog "debug : BOT_PASSWORD ${BOT_PASSWORD} BOT_PORT ${BOT_PORT} BOT_HOSTNAME ${BOT_HOSTNAME}"
+		putlog "::BOTNET_CREATE bots: [bots]"
+		set B_NAME [file rootname [file tail ${FILE_SCT}]]
+		if { "${::botnet-nick}" == "${B_NAME}" || [validuser ${B_NAME}] } { continue }
+		set FILEPIPE		[open "${FILE_SCT}" r]
+		set SCT_DATA 		[split [read ${FILEPIPE}]]
+		close ${FILEPIPE}
+		set BOT_ISMASTER	[lindex ${SCT_DATA} 0]
+		set BOT_PASSWORD	[lindex ${SCT_DATA} 1]
+		set BOT_PORT		[lindex ${SCT_DATA} 2]
+		set BOT_HOSTNAME	[lindex ${SCT_DATA} 3]
+		set BOT_LASTMOD		[lindex ${SCT_DATA} 4]
+		putlog "debug : ${SCT_DATA}"
+		putlog "debug : BOT_ISMASTER ${BOT_ISMASTER} BOT_HOSTNAME ${BOT_HOSTNAME}"
 
-    if {$BOT_ISMASTER == 1 && $::EGG_ISMASTER == 0} {
-      	putlog "Ajout HUB addbot ${B_NAME} ${BOT_HOSTNAME} ${BOT_PORT}"
-      	deluser ${B_NAME}
-      	addbot ${B_NAME} ${BOT_HOSTNAME} ${BOT_PORT}
-      	botattr ${B_NAME} +h
-      	setuser ${B_NAME} XTRA EGG_LASTMOD ${BOT_LASTMOD}
-    } elseif {$BOT_ISMASTER == 0 && $::EGG_ISMASTER == 1} {
-      	putlog "Ajout LEAF addbot ${B_NAME} ${BOT_HOSTNAME}"
-      	deluser ${B_NAME}
-      	addbot ${B_NAME} ${BOT_HOSTNAME}
-      	botattr ${B_NAME} +l
-      	setuser ${B_NAME} XTRA EGG_LASTMOD ${BOT_LASTMOD}
-    } elseif {[validuser ${B_NAME}] == 1 && ${BOT_LASTMOD} != [getuser ${B_NAME} XTRA EGG_LASTMOD]} {
-		putlog "Changement mot de passe ${B_NAME}"
-      	setuser ${B_NAME} PASS ${BOT_PASSWORD}
-    }
-
-    catch {link ${B_NAME}}
-  }
+		if {$BOT_ISMASTER == 1 && $::EGG_ISMASTER == 0} {
+			putlog "Ajout HUB addbot ${B_NAME} ${BOT_HOSTNAME} ${BOT_PORT}"
+			deluser ${B_NAME}
+			addbot ${B_NAME} ${BOT_HOSTNAME} ${BOT_PORT}
+			botattr ${B_NAME} +h
+			setuser ${B_NAME} XTRA EGG_LASTMOD ${BOT_LASTMOD}
+		} elseif {$BOT_ISMASTER == 0 && $::EGG_ISMASTER == 1} {
+			putlog "Ajout LEAF addbot ${B_NAME} ${BOT_HOSTNAME}"
+			deluser ${B_NAME}
+			addbot ${B_NAME} ${BOT_HOSTNAME}
+			botattr ${B_NAME} +l
+			setuser ${B_NAME} XTRA EGG_LASTMOD ${BOT_LASTMOD}
+		} elseif {[validuser ${B_NAME}] == 1 && ${BOT_LASTMOD} != [getuser ${B_NAME} XTRA EGG_LASTMOD]} {
+			putlog "Changement mot de passe ${B_NAME}"
+			setuser ${B_NAME} PASS ${BOT_PASSWORD}
+		} else {
+			putlog "aucune action pour ${B_NAME}"
+		}
+		catch {link ${B_NAME}}
+	}
       catch {save}
 }
 # Call the "BOTNET_CREATE" proc after 5 seconds
