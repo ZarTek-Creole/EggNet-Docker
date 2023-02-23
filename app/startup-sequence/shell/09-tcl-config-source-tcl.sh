dcecdf472e9b0ecb9d36d13018a54c985ea940bc
ZarTek-Creole <ZarTek.Creole@gmail.com>
2023-02-23 14:59:09 +0000

    Fix error for errorInfo

--- app/startup-sequence/shell/09-tcl-config-source-tcl.sh
+++ app/startup-sequence/shell/09-tcl-config-source-tcl.sh
@@ -7,7 +7,7 @@ echo "proc LoadDirectoryFiles { Directory {Extention *.tcl} {Load_Name \"\"} } {
 		if { [file readable \${FILE_SCRIPT}] } {
 			putlog \"\${Load_Name} script sourced: \${FILE_SCRIPT}\";
 			if { [catch {source \${FILE_SCRIPT}} error] } {
-				foreach { errorline } [split \$::errorInfo \"n\"] {
+				foreach { errorline } [split \$::errorInfo \"\\n\"] {
 					putlog \${errorline};
 				};
 				die \"Error in '\${FILE_SCRIPT}' file. Return:\$error\";
@@ -16,16 +16,16 @@ echo "proc LoadDirectoryFiles { Directory {Extention *.tcl} {Load_Name \"\"} } {
 			putlog \"Le fichier '\${FILE_SCRIPT}' ne peut pas être lus\";
 		}
 	}
-}" >> "${EGG_PATH_CONF}/${EGG_LONG_NAME}.conf"
+}" >>"${EGG_PATH_CONF}/${EGG_LONG_NAME}.conf"
 
 # script docker
-echo "LoadDirectoryFiles /startup-sequence/tcl *.tcl Docker" >> "${EGG_PATH_CONF}/${EGG_LONG_NAME}.conf"
+echo "LoadDirectoryFiles /startup-sequence/tcl *.tcl Docker" >>"${EGG_PATH_CONF}/${EGG_LONG_NAME}.conf"
 
 # script global
-echo "LoadDirectoryFiles ${EGG_PATH_SCRIPTS} *.tcl Global" >> "${EGG_PATH_CONF}/${EGG_LONG_NAME}.conf"
+echo "LoadDirectoryFiles ${EGG_PATH_SCRIPTS} *.tcl Global" >>"${EGG_PATH_CONF}/${EGG_LONG_NAME}.conf"
 
 # scripts Network
-echo "LoadDirectoryFiles ${EGG_PATH_SCRIPTS}/${IRC_NETNAME} *.tcl Network" >> "${EGG_PATH_CONF}/${EGG_LONG_NAME}.conf"
+echo "LoadDirectoryFiles ${EGG_PATH_SCRIPTS}/${IRC_NETNAME} *.tcl Network" >>"${EGG_PATH_CONF}/${EGG_LONG_NAME}.conf"
 
 # scripts user
-echo "LoadDirectoryFiles ${EGG_PATH_SCRIPTS}/${IRC_NETNAME}/${EGG_NICK} *.tcl User" >> "${EGG_PATH_CONF}/${EGG_LONG_NAME}.conf"
+echo "LoadDirectoryFiles ${EGG_PATH_SCRIPTS}/${IRC_NETNAME}/${EGG_NICK} *.tcl User" >>"${EGG_PATH_CONF}/${EGG_LONG_NAME}.conf"
--- app/startup-sequence/tcl/04-bgerror.tcl
+++ app/startup-sequence/tcl/04-bgerror.tcl
@@ -1,11 +1,12 @@
+# 
 proc bgerror { message } {
 	set timestamp	[clock format [clock seconds]]
-	set FILE_PIPE	[open ${::EGG_PATH_LOGS}/${::EGG_LONG_NAME}_error.log {WRONLY CREAT APPEND}]
-	set LASTPROC	[info level 0]
-	puts ${FILE_PIPE} "${timestamp}: bgerror in ${LASTPROC} '${message}'";
+	set filePipe	[open ${::EGG_PATH_LOGS}/${::EGG_LONG_NAME}_error.log {WRONLY CREAT APPEND}]
+	set lastProc	[info level 0]
+	puts ${filePipe} "${timestamp}: bgerror in ${lastProc} '${message}'";
 	foreach err [split ${::errorInfo} "\n"] {
-		puts ${FILE_PIPE} ${err}
+		puts ${filePipe} ${err}
 		putallbots "NET_ERROR ${::EGG_LONG_NAME} ${err}"
 	}
-	close ${FILE_PIPE}
+	close ${filePipe}
 }
